1fe74fc649c9e9b44e1e8037b611a4cfc5837fc0
TheJKL <vanjackal@protonmail.com>
2020-09-05 12:56:01 -0500

    initial commit and some work on player controller

---
+++ Assets/Scripts/PlayerLook.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+    public Camera camera;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Vector2 mousePos = camera.ViewportToWorldPoint(Input.mousePosition);
+
+    }
+}
---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+
+    public float moveForce;
+    public float maxSpeed;
+
+    void Update()
+    {
+        Rigidbody2D playerBody = gameObject.GetComponent<Rigidbody2D>();
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        Vector2 move = transform.right * x + transform.up * y;
+
+        if(playerBody.velocity.magnitude < maxSpeed)
+        {
+            playerBody.AddForce(move * moveForce);
+        } 
+    }
+}
