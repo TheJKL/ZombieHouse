c1a1842d12a91571cd4d0fd485d33874046515da
TheJKL <vanjackal@protonmail.com>
2020-09-06 10:03:38 -0500

    added basic enemy with health, bullets now do damage and die when the collide with anything

---
+++ Assets/Scripts/bullet.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class bullet : MonoBehaviour
+{
+    public LayerMask noHit;
+    public float damage;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("hit");
+        zombie hit = collision.gameObject.GetComponent<zombie>();
+        if(hit != null)
+        {
+            hit.damage(damage);
+        }
+        if ((noHit.value & (1 << collision.gameObject.layer)) == 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/zombie.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class zombie : MonoBehaviour
+{
+    public float health;
+    void Update()
+    {
+        
+    }
+
+    public void damage(float dmg)
+    {
+        health -= dmg;
+        if(health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
