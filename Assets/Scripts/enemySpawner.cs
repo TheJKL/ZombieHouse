02b6c0b39e24b5b7f8caf78c2971af722adc83e4
TheJKL <vanjackal@protonmail.com>
2020-09-27 13:01:20 -0500

    added a cooldown to the spawner

--- Assets/Scripts/enemySpawner.cs
+++ Assets/Scripts/enemySpawner.cs
@@ -4,9 +4,26 @@ public class enemySpawner : MonoBehaviour
 {
     public GameObject enemyType;
     public GameObject player;
+    public float spawnTime;
+    private float nextSpawnTime;
+
+    private void Start()
+    {
+        nextSpawnTime = Time.time;
+    }
+
     void Update()
     {
-        GameObject zombie = Instantiate(enemyType,transform);
+        if (Time.time >= nextSpawnTime)
+        {
+            spawn();
+        }
+    }
+
+    public void spawn()//TODO move spawn control to a seperate object (to allow for different types of spawners)
+    {
+        GameObject zombie = Instantiate(enemyType, transform);
         zombie.GetComponent<zombie>().setTarget(player);
+        nextSpawnTime = Time.time + spawnTime;
     }
 }
