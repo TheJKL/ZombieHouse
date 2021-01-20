6e607aa171161dee1da79c5402115594124ea834
TheJKL <vanjackal@protonmail.com>
2021-01-20 14:29:04 -0600

    added random window choice and increased damage for testing Closes #3

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -3,12 +3,24 @@ using System.Collections.Generic;
 
 public class GameController : MonoBehaviour
 {
-
+    [HideInInspector]
     public List<enemySpawner> spawners;
+    public float spawnAttemptInterval;
+    private float nextSpawnTime;
+
+    void Start()
+    {
+        nextSpawnTime = Time.time + spawnAttemptInterval;
+    }
 
     void Update()
     {
-        
+        if (nextSpawnTime < Time.time)
+        {
+            int idx = Random.Range(0, spawners.Count);
+            spawners[idx].attemptSpawn();
+            nextSpawnTime = Time.time + spawnAttemptInterval;
+        }
     }
 
     public void addSpawner(enemySpawner spawner)
--- Assets/Scripts/enemySpawner.cs
+++ Assets/Scripts/enemySpawner.cs
@@ -5,67 +5,40 @@ public class enemySpawner : MonoBehaviour
     public GameObject enemyType;
     public GameObject player;
     public GameObject windowBoard;
-    public float spawnTime;
-    private float nextSpawnTime;
     public int maxBoards;
     private int boards;
 
     private void Start()
     {
-        nextSpawnTime = Time.time + spawnTime;
         boards = maxBoards;
 
         GameObject.FindWithTag("GameController").GetComponent<GameController>().addSpawner(this); //add spawner to controller list
     }
 
-    void Update()
-    {
-        bool offCooldown = Time.time >= nextSpawnTime;
-
-        if (offCooldown && boards == 0)
-        {
-            spawn();
-        }
-        else if (offCooldown && boards > 0)
-        {
-            boards--;
-            resetCooldown();
-            Debug.Log($"Boards Removed, Boards = {boards}");
-            updateBoard();
-        }
-    }
-
-    private void OnTriggerStay2D(Collider2D collision)//This may need to be moved into the player object (centralize the control)
+    private void OnTriggerStay2D(Collider2D collision)//TODO This may need to be moved into the player object (centralize the control)
     {
         if(boards < maxBoards)
         {
             boards++;
-            updateBoard();
+            windowBoard.SetActive(true);
             Debug.Log($"Boards Added, Boards = {boards}");
         }
     }
 
-    public void spawn()//TODO move spawn control to a seperate object (to allow for different types of spawners)
+    public void attemptSpawn()
     {
+        Debug.Log("Spawn Attempted");
+        if(boards > 1)
+        {
+            boards--;
+            return;
+        } 
+        else if (boards == 1)
+        {
+            boards--;
+        }
+        windowBoard.SetActive(false);
         GameObject zombie = Instantiate(enemyType, transform);
         zombie.GetComponent<zombie>().setTarget(player);
-        resetCooldown();
-    }
-
-    public void resetCooldown()
-    {
-        nextSpawnTime = Time.time + spawnTime;
-    }
-
-    public void updateBoard()
-    {
-        if(boards > 0)
-        {
-            windowBoard.SetActive(true);
-        }
-        else
-        {
-            windowBoard.SetActive(false);
-        }
     }
 }
