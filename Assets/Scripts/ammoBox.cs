fe7ab97f764388c8c41680b51ab1426146349134
TheJKL <vanjackal@protonmail.com>
2021-01-22 08:35:49 -0600

    added ammo boxes. Closes #4

---
+++ Assets/Scripts/ammoBox.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class ammoBox : MonoBehaviour
+{
+
+    public int ammo;
+
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        collision.GetComponent<PlayerStats>().incrementAmmo(ammo);
+        Destroy(gameObject);
+    }
+}
