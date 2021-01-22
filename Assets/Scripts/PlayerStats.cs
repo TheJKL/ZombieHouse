373ea5f904cf140945eeee58072156c1f0fe4d5c
TheJKL <vanjackal@protonmail.com>
2021-01-22 07:49:08 -0600

    added health and ammo counters

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -1,22 +1,43 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerStats : MonoBehaviour
 {
     
     public float maxHealth;
     private float health;
+    public float maxAmmo;
+    public float startAmmo;
+    private float ammo;
+    public Text healthUI;
+    public Text ammoUI;
 
     private void Start()
     {
+        ammo = startAmmo;
         health = maxHealth;
+        incrementAmmo(0);
+        updateHealth();
     }
 
     public void damage(float dmg)
     {
         health -= dmg;
+        updateHealth();
         if(health <= 0)
         {
             Destroy(gameObject);
         }
+    }
+
+    public void incrementAmmo(int increment)
+    {
+        ammo += increment;
+        ammoUI.text = $"{ammo}/{maxAmmo}";
+    }
+
+    public void updateHealth()
+    {
+        healthUI.text = $"{health}/{maxHealth}";
     }
 }
